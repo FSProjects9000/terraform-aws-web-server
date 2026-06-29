f425ba58ec41482c81dcffcbd56ef5f7afa68726
minipikachu800fs-beep <minipikachu800fs@gmail.com>
2026-06-29 16:38:37 +0100

    modified:   terraform/main.tf 	deleted:    terraform/outputs.tf 	deleted:    terraform/provider.tf 	deleted:    terraform/variables.tf 	deleted:    terraform/versions.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,24 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = ">5.92"
+            version = ">6.0"
         }
     }
+}
 
-    required_version = ">= 1.2"
+provider "aws"{
+    region = "eu-west-3"
+}
+
+
+ # Resource blocks declare a resource type and name. 
+ # Together, the type and name form a resource identifier (ID)
+ # in the format resource_type.resource_name, 
+ # in this case aws_vpn.example
+
+ # Resource types always start with the provider name followed by an 
+ # underscore. The aws_vpc resource type belongs to the aws provider
+
+resource "aws_vpc" "example" {
+    cidr_block = "10.0.0.0/16"
 }
