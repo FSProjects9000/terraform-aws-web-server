7fa7c446f9df8c672f3899b8843c06cfa3ae5920
minipikachu800fs-beep <minipikachu800fs@gmail.com>
2026-08-05 21:51:16 +0100

    deletion of comments

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bootstrap"
-    key            = "portfolio/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
--- terraform/network.tf
+++ terraform/network.tf
@@ -11,15 +11,6 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-
-# Resource blocks declare a resource type and name. 
-# Together, the type and name form a resource identifier (ID)
-# in the format resource_type.resource_name, 
-# in this case aws_vpn.example
-
-# Resource types always start with the provider name followed by an 
-# underscore. The aws_vpc resource type belongs to the aws provider
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
