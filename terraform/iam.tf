cbb028fce93a160f90481af0be8f177c85fdd8c8
minipikachu800fs-beep <minipikachu800fs@gmail.com>
2026-08-18 10:36:40 +0100

    fix

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role" "ec2_role" {
 
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role" "ssm_maintenance_window" {
